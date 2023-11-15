910dfd5e507c86c398f70b3f8173e070705e913a
rezjsh <149146755+rezjsh@users.noreply.github.com>
2023-11-15 17:21:53 +0330

    install coockie-parser and add to server.js use urlencoded

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectToMongoDB from "./db/mongoDB.js";
 import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 // Create Express app
@@ -11,7 +13,9 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 // Connect to MongoDB
 connectToMongoDB();
